63f2c424cd2a9ed5571911dcbe7e56d947b73bf6
Mraprguild8133 <mraprguild06@gmail.com>
2025-11-09 12:15:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,6 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+from os import environ
 
-class Config:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    
-    # qBittorrent settings
-    QBIT_HOST = os.getenv("QBIT_HOST", "http://localhost:8080")
-    QBIT_USERNAME = os.getenv("QBIT_USERNAME", "admin")
-    QBIT_PASSWORD = os.getenv("QBIT_PASSWORD", "adminadmin")
-    
-    # Download settings
-    MAX_FILE_SIZE = 4 * 1024 * 1024 * 1024  # 4GB
-    DOWNLOAD_PATH = "downloads"
-    UPLOAD_PATH = "uploads"
-
-config = Config()
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
